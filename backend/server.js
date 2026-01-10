ed0c2686aa649b83279a3ad8911805b83a52a357
ka1031 <ak.aayush04@gmail.com>
2026-01-10 18:59:46 +0530

    Add PostgreSQL integration and update gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,39 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running ");
+  res.send("Backend is running");
 });
 
-app.post("/Login", (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  if (email === "admin@test.com" && password === "1234") {
-    res.json({ success: true });
-  } else {
-    res.status(401).json({ success: false });
+  try {
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ success: false });
+    }
+
+    const user = result.rows[0];
+
+    if (user.password === password) {
+      res.json({ success: true });
+    } else {
+      res.status(401).json({ success: false });
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
   }
 });
 
@@ -23,4 +41,3 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
